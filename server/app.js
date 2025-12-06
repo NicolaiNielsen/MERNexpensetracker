9e9cc2469ff6ffef87372157f8bcf8456cea80a6
Nicolai Nielsen <Nicolaiaphichat@gmail.com>
2025-12-06 15:40:58 +0100

    wip

--- server/app.js
+++ server/app.js
@@ -15,13 +15,21 @@
 // No interpreter in the middle. The CPU can execute it directly, with fewer checks and jumps.
 
 // Result: much faster execution.
-const express = require('express'); // CommonJS 
+const express = require('express'); // CommonJS
+
+//App  express is an object that gives us evertyhing we need to build a webserver in node
+//allows us to define routes. app.get or app.post
+// attach middleware app.use cpmtrpæs wjat ajeååems nweween the request and routes.
+// for instance prefixes our '/api/v2/expenses' requests to 
+
+//
+const expenseRouter = require("./routers/expenseRouters") 
 require('dotenv').config();
 
 const app = express(); // creates express application cd instance like a class?
 
 //API Routes
-app.use('/api/v2/expenses', )
+app.use('/api/v2/expenses', expenseRouter)
 
 app.get('/', (req, res) => { // basicially an empty http
     console.log("Work");
